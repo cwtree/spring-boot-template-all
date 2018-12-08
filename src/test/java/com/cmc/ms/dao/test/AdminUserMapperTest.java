2fc26f65ea7427bf5699f304b61be0233edbe35b
Simon Chi <719259043@qq.com>
2018-12-08 18:57:20 +0800

    update
    update

--- src/test/java/com/cmc/ms/dao/test/AdminUserMapperTest.java
+++ src/test/java/com/cmc/ms/dao/test/AdminUserMapperTest.java
@@ -3,13 +3,6 @@
  * 
  * File Created at 2017-8-16
  * 
- * Copyright 2016 CMCC Corporation Limited.
- * All rights reserved.
- *
- * This software is the confidential and proprietary information of
- * ZYHY Company. ("Confidential Information").  You shall not
- * disclose such Confidential Information and shall use it only in
- * accordance with the terms of the license.
  */
 package com.cmc.ms.dao.test;
 
