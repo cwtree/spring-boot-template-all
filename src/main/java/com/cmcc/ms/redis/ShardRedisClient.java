b53ab324269696c79a838d504bdc6773f5148909
Simon Chi <719259043@qq.com>
2018-12-08 18:55:50 +0800

    update
    update

--- src/main/java/com/cmcc/ms/redis/ShardRedisClient.java
+++ src/main/java/com/cmcc/ms/redis/ShardRedisClient.java
@@ -3,13 +3,6 @@
  * 
  * File Created at 2016年6月13日
  * 
- * Copyright 2016 CMCC Corporation Limited.
- * All rights reserved.
- *
- * This software is the confidential and proprietary information of
- * ZYHY Company. ("Confidential Information").  You shall not
- * disclose such Confidential Information and shall use it only in
- * accordance with the terms of the license.
  */
 package com.cmcc.ms.redis;
 
